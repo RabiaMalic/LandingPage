2312dbccab6c4ee0efe20ddfe40e151990d924de
RabiaMalic <141947744+RabiaMalic@users.noreply.github.com>
2023-08-10 10:03:14 -0700

    Add files via upload

--- LandingPage/app.js
+++ LandingPage/app.js
@@ -3,18 +3,3 @@ const navlist=document.getElementById("navlist")
 hamburger.addEventListener("click",()=>{
     navlist.classList.toggle("navlist-active")
 })
-const hamurger=document.getElementById("hamburger")
-const nab=document.getElementById("navlist")
-hamburger.addEventListener("click",()=>{
-    navlist.classList.toggle("navlist-active")
-})
-const hamburge=document.getElementById("hamburger")
-const navlst=document.getElementById("navlist")
-hamburger.addEventListener("click",()=>{
-    navlist.classList.toggle("navlist-active")
-})
-const hambuger=document.getElementById("hamburger")
-const navlit=document.getElementById("navlist")
-hamburger.addEventListener("click",()=>{
-    navlist.classList.toggle("navlist-active")
-})
